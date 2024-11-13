102df4c3bcb13b4c889f4e38767b0d303fe8b34e
Saint. Clever <parchment@gmx.com>
2024-11-13 09:45:03 -0500

    Add middleware pino logger

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import pino from 'pino';
 
 // __dirname
 import { fileURLToPath } from 'url';
@@ -18,13 +19,22 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extend: true }))
+const logger = pino();
 
+
+// Data
 const data = {
   "user": "Saint. Clever",
-  "language": "JavaScript",
+  "language": "Python",
   "image": "https://avatars.githubusercontent.com/u/8880726?v=4"
 }
 
+const visiters = [];
+
+// Routes
 app.get('/', (req, res) => {
   // Can exclude .ejs from index.ejs
   res.render('index', data);
@@ -34,7 +44,15 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+app.post('/contact', (req, res) => {
+  console.log(req.body);
+  visiters.push(req.body)
+  logger.info(visiters)
+  res.send('Post succesfully ', 201);
+});
+
 app.listen(PORT, (err) => {
   if (err) console.error(err);
+  logger.info('http://localhost:3000');
   console.log('Server listening on PORT:', PORT);
 });
