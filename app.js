949055f0f3beaf5aa1758f5a052d6512792cd2ec
Saint. Clever <parchment@gmx.com>
2024-11-12 20:14:55 -0500

    Add links and update css

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import ejs from 'ejs';
 
 // __dirname
 import { fileURLToPath } from 'url';
@@ -9,16 +8,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
+// Express instance
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+// EJS setup
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
 
+
+const data = {
+  "user": "Saint. Clever",
+  "language": "JavaScript",
+  "image": "https://avatars.githubusercontent.com/u/8880726?v=4"
+}
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  // Can exclude .ejs from index.ejs
+  res.render('index', data);
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 app.listen(PORT, (err) => {
